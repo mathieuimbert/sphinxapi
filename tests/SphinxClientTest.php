ecf681a7aa61fbb41a1d50f9c73351165e7fc109
Mathieu Imbert <mimbert@nexweb.ca>
2016-08-17 11:11:01 -0400

    Implement phpunit

---
+++ tests/SphinxClientTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace MathieuImbert\Sphinx\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SphinxClientTest extends TestCase
+{
+
+    public function testTrue()
+    {
+        $var = 0;
+        $this->assertEquals(0, $var);
+    }
+
+}
